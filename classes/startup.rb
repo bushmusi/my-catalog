9d30c1353f35e94510723b3f8e815cf2b56504fa
ZahraArshia <78906545+ZahraArshia@users.noreply.github.com>
2022-05-30 21:20:36 +0430

    implemented start up actions

---
+++ classes/startup.rb
@@ -0,0 +1,35 @@
+class Startup
+  def options_list
+    puts ''
+    puts "Please choose an option by selecting a number:
+    1. List all books
+    2. List all musics
+    3. List of games
+    4. Add a book
+    5. Add a music
+    6. Add a game
+    7. Exit"
+    gets.chomp
+  end
+
+  loop do
+    case options_list
+    when '1'
+      booklist
+    when '2'
+      musiclist
+    when '3'
+      gamelist
+    when '4'
+      create_book
+    when '5'
+      create_music
+    when '6'
+      create_game
+    when '7'
+      exit
+    else
+      puts 'Invalid input. Try again'
+    end
+  end
+end
