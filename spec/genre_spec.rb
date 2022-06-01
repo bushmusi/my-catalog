a97a19b4275276105f7870ffd83981ed8cc04052
usorfaitheloho <usorfaitheloho@gmail.com>
2022-06-01 15:56:33 -0700

    Add test for genre

---
+++ spec/genre_spec.rb
@@ -0,0 +1,38 @@
+require_relative '../classes/item'
+require_relative '../classes/genre'
+
+describe Genre do
+  context 'it should work for the genre class' do
+    before(:all) do
+      @genre = Genre.new('Gospel music')
+    end
+
+    it 'should return "@genre" as an instance variable of Genre class' do
+      expect(@genre).to be_an_instance_of Genre
+    end
+
+    it 'should return attribute name of music genre' do
+      expect(@genre.name).to eq 'Gospel music'
+    end
+
+    it 'should check a typo error of the attribute name of music genre' do
+      expect(@genre.name).not_to eq 'Gospel musics'
+    end
+
+    it 'should return an empty array as the default value of "@items"' do
+      expect(@genre.items).to eq []
+    end
+
+    it 'should return the length of "@items"' do
+      expect(@genre.items.length).to eq 0
+    end
+
+    it 'should add an item in items list' do
+      item = Item.new('2022-06-01')
+      @genre.add_item(item)
+      expect(@genre.items.length).to eq 1
+      expect(@genre.items).to eq [item]
+      expect(item.genre).to eq @genre
+    end
+  end
+end
