e57cf13a7cc691e915f4658cebd658fadcee0aac
Bushra Mustofa <bushera.mestofa@gmail.com>
2022-06-03 21:23:21 +0300

    Remove unused arg

--- classes/book-store/book.rb
+++ classes/book-store/book.rb
@@ -3,8 +3,8 @@ require_relative '../item'
 class Book < Item
   attr_accessor :publisher, :cover_state, :label_id, :id
 
-  def initialize(date, publisher, cover_state)
-    super(date)
+  def initialize(publisher, cover_state)
+    super()
     @id = Random.rand(1..10_000)
     @publisher = publisher
     @cover_state = cover_state.to_s
